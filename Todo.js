b8bb9c93346d253d7cda39eac5f6bd1e46d615de
jesna <jasna.edava@gmail.com>
2021-02-19 00:35:42 -0800

    first commit : first version

---
+++ Todo.js
@@ -0,0 +1,23 @@
+function todolist() {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+
+      if (this.readyState == 4 && this.status == 200) {
+          var response = JSON.parse(this.responseText); 
+          var output= "";
+          for(var i=0;i<response.length;i++){
+          output +=  "<br>"+"<li>" + response[i].title +"</li>"; 
+        
+      }
+      document.getElementById("demo").innerHTML =output;
+
+    }
+}
+
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+    xhttp.send();
+}
+
+
+  
