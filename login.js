2ffa356a7e6cf3050f1b11db7560fe9bcc16e551
jesna123 <78085934+jesna123@users.noreply.github.com>
2021-04-16 08:45:51 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,1 @@
-let username = document.getElementById("username");
-let pass  = document.getElementById("pass");
-function validation(){
-    // if(username.value==""||pass.value==""){
-    //     alert("fields cannot be empty");
-    //     return false;
-    
-    if(username.value!="admin"||pass.value!="12345")
-    {alert("username or password is invalid");
-   return false;
-}
-else{
-    return true;
-}
-}
+
